acfd360dac34ef689ff7d05ea683f41d4699e4c6
Alex Pinchuk <apinchuk@verisk.com>
2015-11-19 14:37:35 -0800

    Fixed empty trailing in facts

--- lib/rhsm_repos.rb
+++ lib/rhsm_repos.rb
@@ -3,7 +3,7 @@ Facter.add('rhsm_repos') do
     repos = Array.[]
     repo_list = Facter::Core::Execution.exec('subscription-manager repos --list-enabled | grep "Repo ID" | cut -d " " -f 5')
     repo_list.each_line do |line|
-      repos.push(line)
+      repos.push(line.strip)
     end 
     repos
   end

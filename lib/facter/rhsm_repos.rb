16a81a009e2b5ad5313150d476bd98bc3658e0a0
Benjamin Merot <bem.its@dsg.dk>
2016-08-18 14:56:35 +0200

    Fixing surrounding quotes around command

--- lib/facter/rhsm_repos.rb
+++ lib/facter/rhsm_repos.rb
@@ -1,7 +1,7 @@
 Facter.add('rhsm_repos') do
   setcode do
     repos = Array.[]
-    repo_list = Facter::Core::Execution.exec('yum repolist | awk '/Red Hat/ {print $1}'')
+    repo_list = Facter::Core::Execution.exec("yum repolist | awk '/Red Hat/ {print $1}'")
     repo_list.each_line do |line|
       repos.push(line.strip)
     end

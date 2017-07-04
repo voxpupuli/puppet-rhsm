e328f3fdd96af83cab14f5e022c8528404be6444
Benjamin Merot <bem.its@dsg.dk>
2017-07-04 11:39:34 +0200

    Added confine rule on Fact

--- lib/facter/rhsm_repos.rb
+++ lib/facter/rhsm_repos.rb
@@ -1,4 +1,5 @@
 Facter.add('rhsm_repos') do
+  confine :osfamily => :RedHat
   setcode do
     repos = Array.[]
     repo_list = Facter::Core::Execution.exec("yum repolist | awk '/Red Hat/ {print $1}'")

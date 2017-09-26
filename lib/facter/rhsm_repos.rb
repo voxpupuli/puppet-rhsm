90b67c38e859db350cf180d29558bd50aee3fb9f
Lukas Kallies <kallies@puzzle.ch>
2017-09-26 12:43:26 +0200

    Fix for #52, uses subscription-manager instead of yum

--- lib/facter/rhsm_repos.rb
+++ lib/facter/rhsm_repos.rb
@@ -2,7 +2,7 @@ Facter.add('rhsm_repos') do
   confine osfamily: :RedHat
   setcode do
     repos = Array.[]
-    repo_list = Facter::Core::Execution.exec("yum repolist | awk '/Red Hat/ {print $1}'")
+    repo_list = Facter::Core::Execution.exec("subscription-manager repos --list-enabled | awk '/Repo ID:/ {print $3}'")
     repo_list.each_line do |line|
       repos.push(line.strip)
     end
